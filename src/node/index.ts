06e5a73386d177fc631f008ac94e58f7fdb07f37
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2024-08-14 16:06:18 -0400

    Better test script

--- src/node/index.ts
+++ src/node/index.ts
@@ -2,12 +2,18 @@ import * as fs from "node:fs";
 import { packRsc, unpackRsc } from "../index.js";
 import { assert } from "../util.js";
 
-const file = fs.readFileSync("./data/yogstation.rsc");
+const path = "./data/paradise.rsc";
+const file = fs.readFileSync(path);
 
 console.time("RSC Parse");
-const rsc = unpackRsc(file.buffer);
+const rsc = unpackRsc(file.buffer, {
+  validate_checksums: false,
+  include_empty: true,
+});
 console.timeEnd("RSC Parse");
 
 const newFile = Buffer.from(packRsc(rsc));
-fs.writeFileSync("./data/yogstation_repacked.rsc", newFile);
+fs.writeFileSync("./data/paradise.rsc.repacked", newFile);
 assert(file.equals(newFile));
+
+console.log(`[SUCCESS] pack(unpack(${path})) is a noop!`);
